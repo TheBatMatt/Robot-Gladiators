71dcd171ae31e4a8556c481ae65a774c2598c3e4
Ted J <tedmjablonowski@gmail.com>
2022-02-11 08:31:39 -0500

    Initial Game Setup!

--- assets/JS/game.js
+++ assets/JS/game.js
@@ -1,1 +1,120 @@
-window.alert("This is an alert! JavaScript is running!");
+// This is a comment
+
+// var = variable which is playerName
+
+var playerName = window.prompt("What is your robot's name?");
+
+var playerHealth = 100;
+var playerAttack = 10;
+var playerMoney = 10;
+
+// You can also log multiple values at once like this
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Vampire Bot";
+var enemyHealth = 50;
+var enemyAttack = 15;
+
+// create function
+var fight = function() {
+    window.alert("Welcome to Robot Gladiators!")
+    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+    console.log(promptFight);
+};
+
+  // Below Execute function
+fight();
+// if player choses to fight, then fight
+if (promptFight === "fight" || promptFight === "FIGHT") {
+
+//Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+enemyHealth = enemyHealth - playerAttack;
+
+// Log a resulting message to the console so we know that it worked.
+console.log(
+  playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+);
+
+// Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable
+playerHealth = playerHealth - enemyAttack;
+
+// Log a resulting message to the console so we know that it worked.
+console.log(
+  enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+);
+
+// check enemy's health
+if (enemyHealth <= 0) {
+    window.alert(enemyName + " has fallen in the mechanical smackdown!");
+  } 
+  else {
+    window.alert(enemyName + " still has " + enemyHealth + " health left.");
+  }
+
+  // check player's health
+if (playerHealth <= 0) {
+    window.alert(playerName + " has died!");
+  } 
+  else {
+    window.alert(playerName + " still has " + playerHealth + " health left.");
+  }
+
+    // if player choses to skip
+} else if (promptFight === "skip" || promptFight === "SKIP") {
+      // confirm player wants to skip
+  var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+    // if yes (true), leave fight
+    if (confirmSkip) {
+  window.alert(playerName + " has decided to skip this fight. Goodbye!");
+  // subtract money from playerMoney for skipping
+  playerMoney = playerMoney - 2;
+  window.alert(playerName + "-2 Money" )
+}
+
+  // if no (false), ask question again by running fight() again
+  else {
+    fight();
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Note the lack of quotation marks around playerName
+//console.log(playerName);
+
+//console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+//console.log(10 + 10);
+// what is this?
+//console.log("Our robot's name is " + playerName);
+
+// A function is a predefined action that we can call or invoke in our code (after we declare it earlier in the code). Fight is the name..
+
+//function fight() {
+ //   window.alert ("The fight has begun!");
+//}
+
+
